0a9e984895aa1726ad34a6e56135438334f72f64
Joshua Murray <murray.joshua@gmail.com>
2017-04-25 12:31:39 -0400

    conf.py changes

--- docs/conf.py
+++ docs/conf.py
@@ -18,6 +18,7 @@
 #
 # import os
 # import sys
+import sphinx_rtd_theme
 # sys.path.insert(0, os.path.abspath('.'))
 
 
@@ -82,7 +83,7 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -175,3 +176,5 @@ epub_copyright = copyright
 epub_exclude_files = ['search.html']
 
 
+html_theme = "sphinx_rtd_theme"  
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]  
