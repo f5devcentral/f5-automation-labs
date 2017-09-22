a6a403bceae87a91487cbc0009d389130e65d652
Nicolas Menant <menant.nicolas@gmail.com>
2017-09-23 00:51:05 +0200

    fixed errors

--- lab_environments/snops_jumphost_setup.sh
+++ lab_environments/snops_jumphost_setup.sh
@@ -7,8 +7,9 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get -y update 
 sudo apt-get -y install google-chrome-stable
+sudo mkdir /home/ubuntu/Desktop
 sudo sh -c 'echo "[Desktop Entry]\nVersion=1.0\nName=Chrome\nComment=Open Chrome\nExec=/opt/google/chrome/chrome\nIcon=/opt/google/chrome/product_logo_48.png\nTerminal=false\nType=Application\nCategories=Internet;Application;\n" > /home/ubuntu/Desktop/Chrome.desktop'
-chmod +x /home/ubuntu/Desktop/Chrome.desktop
+sudo chmod +x /home/ubuntu/Desktop/Chrome.desktop
 
 
 #Install Postman and add the desktop icon
@@ -17,7 +18,7 @@ sudo tar -xzf postman.tar.gz -C /opt
 rm postman.tar.gz
 sudo ln -s /opt/Postman/Postman /usr/bin/postman
 sudo sh -c 'echo "[Desktop Entry]\nVersion=1.0\nName=Postman\nComment=Open Postman\nExec=/opt/Postman/Postman\nIcon=/opt/Postman/resources/app/assets/icon.png\nTerminal=false\nType=Application\nCategories=Utility;Application;\n" > /home/ubuntu/Desktop/Postman.desktop'
-chmod +x /home/ubuntu/Desktop/Postman.desktop
+sudo chmod +x /home/ubuntu/Desktop/Postman.desktop
 
 #Install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
