83ebdf9773cdf00061ce1463c5380ea797ea21d9
Nicolas Menant <N.Menant@F5.com>
2017-09-23 08:14:03 +0200

    removed sudo privilegies in CFT, stuff should run as root

--- lab_environments/snops_jumphost_setup.sh
+++ lab_environments/snops_jumphost_setup.sh
@@ -2,33 +2,37 @@
 
 #The purpose of this script is to setup the required components for the F5 automation lab
 
+#Retrieve the github repo https://github.com/f5devcentral/f5-automation-labs
+apt-get -y install git
+git clone https://github.com/f5devcentral/f5-automation-labs /home/ubuntu/f5-automation-labs
+
 #Install Chrome setup and add the desktop icon
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
-sudo apt-get -y update 
-sudo apt-get -y install google-chrome-stable
-sudo mkdir /home/ubuntu/Desktop
-sudo sh -c 'echo "[Desktop Entry]\nVersion=1.0\nName=Chrome\nComment=Open Chrome\nExec=/opt/google/chrome/chrome\nIcon=/opt/google/chrome/product_logo_48.png\nTerminal=false\nType=Application\nCategories=Internet;Application;\n" > /home/ubuntu/Desktop/Chrome.desktop'
-sudo chmod +x /home/ubuntu/Desktop/Chrome.desktop
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+apt-get -y update
+apt-get -y install google-chrome-stable
+mkdir /home/ubuntu/Desktop
+sh -c 'echo "[Desktop Entry]\nVersion=1.0\nName=Chrome\nComment=Open Chrome\nExec=/opt/google/chrome/chrome\nIcon=/opt/google/chrome/product_logo_48.png\nTerminal=false\nType=Application\nCategories=Internet;Application;\n" > /home/ubuntu/Desktop/Chrome.desktop'
+chmod +x /home/ubuntu/Desktop/Chrome.desktop
 
 
 #Install Postman and add the desktop icon
 wget https://dl.pstmn.io/download/latest/linux64 -O postman.tar.gz
-sudo tar -xzf postman.tar.gz -C /opt
+tar -xzf postman.tar.gz -C /opt
 rm postman.tar.gz
-sudo ln -s /opt/Postman/Postman /usr/bin/postman
-sudo sh -c 'echo "[Desktop Entry]\nVersion=1.0\nName=Postman\nComment=Open Postman\nExec=/opt/Postman/Postman\nIcon=/opt/Postman/resources/app/assets/icon.png\nTerminal=false\nType=Application\nCategories=Utility;Application;\n" > /home/ubuntu/Desktop/Postman.desktop'
-sudo chmod +x /home/ubuntu/Desktop/Postman.desktop
+ln -s /opt/Postman/Postman /usr/bin/postman
+sh -c 'echo "[Desktop Entry]\nVersion=1.0\nName=Postman\nComment=Open Postman\nExec=/opt/Postman/Postman\nIcon=/opt/Postman/resources/app/assets/icon.png\nTerminal=false\nType=Application\nCategories=Utility;Application;\n" > /home/ubuntu/Desktop/Postman.desktop'
+chmod +x /home/ubuntu/Desktop/Postman.desktop
 
 #Install docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install -y docker-ce
 
 #pull the f5-super-netops images : base, jenkins, ansible
-sudo docker pull f5devcentral/f5-super-netops-container:base
-sudo docker pull f5devcentral/f5-super-netops-container:jenkins
-sudo docker pull f5devcentral/f5-super-netops-container:ansible
+docker pull f5devcentral/f5-super-netops-container:base
+docker pull f5devcentral/f5-super-netops-container:jenkins
+docker pull f5devcentral/f5-super-netops-container:ansible
 
 
