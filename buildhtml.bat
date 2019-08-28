98e5f9eba607cae795eabb24a8ecc3e0ae3b1cd4
vtog <v.tognaci@f5.com>
2019-08-28 15:01:06 -0500

    f5 containthedocs update

--- buildhtml.bat
+++ buildhtml.bat
@@ -1,1 +1,1 @@
-docker run --rm -it -v %cd%:/data -w /data f5devcentral/containthedocs:1.0.8 make -C docs html
+docker run --rm -it -v %cd%:/data -w /data f5devcentral/containthedocs:latest make -C docs html
