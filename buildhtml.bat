cbf9287fa9d99550d1564757ccec95a37de4c289
Vince Tognaci <vtog@users.noreply.github.com>
2019-08-28 15:23:47 -0500

    Update buildhtml.bat

--- buildhtml.bat
+++ buildhtml.bat
@@ -1,1 +1,1 @@
-docker run --rm -it -v %cd%:/data -w /data 0xhiteshpatel/containthedocs:2.3 make -C docs html
+docker run --rm -it -v %cd%:/data -w /data f5devcentral/containthedocs:latest make -C docs html
